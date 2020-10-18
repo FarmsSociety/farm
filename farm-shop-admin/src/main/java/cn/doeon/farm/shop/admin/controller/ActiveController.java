095490358ccdd2f59b1adb55c9a3be4899ba6b04
Brian921 <852863107@qq.com>
2020-10-18 22:43:04 +0800

    新增
    新增

--- farm-shop-admin/src/main/java/cn/doeon/farm/shop/admin/controller/ActiveController.java
+++ farm-shop-admin/src/main/java/cn/doeon/farm/shop/admin/controller/ActiveController.java
@@ -2,10 +2,12 @@ package cn.doeon.farm.shop.admin.controller;
 
 import cn.doeon.farm.shop.bean.common.ResponseResult;
 import cn.doeon.farm.shop.bean.common.ResultMsg;
+import cn.doeon.farm.shop.bean.dto.ActivityInfoDto;
 import cn.doeon.farm.shop.bean.enums.ResultStatus;
 import cn.doeon.farm.shop.bean.model.science.ActivityInfo;
 import cn.doeon.farm.shop.bean.model.science.ActivityPraise;
 import cn.doeon.farm.shop.service.ScienceActivityEvaluateService;
+import cn.doeon.farm.shop.service.ScienceActivityParticipantsService;
 import cn.doeon.farm.shop.service.ScienceActivityPraiseService;
 import cn.doeon.farm.shop.service.ScienceActivityService;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -28,6 +30,8 @@ public class ActiveController {
     @Autowired
     private ScienceActivityService scienceActivityService;
     @Autowired
+    private ScienceActivityParticipantsService scienceActivityParticipantsService;
+    @Autowired
     private ScienceActivityEvaluateService scienceActivityEvaluateService;
     @Autowired
     private ScienceActivityPraiseService scienceActivityPraiseService;
@@ -36,13 +40,14 @@ public class ActiveController {
      *
      * @return
      */
-    @ApiOperation(value = "获取活动列表", notes = "获取活动列表接口")
+    @ApiOperation(value = "获取活动招募列表", notes = "获取活动招募列表")
     @GetMapping("/list")
     public ResponseResult<IPage<ActivityInfo>> getActivityList(@RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo,
-                                                               @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+                                                               @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize,
+                                                               ActivityInfoDto activityInfo) {
         ResponseResult<IPage<ActivityInfo>> result = new ResponseResult<>();
         Page<ActivityInfo> page = new Page<>(pageNo, pageSize);
-        IPage<ActivityInfo> activityList = scienceActivityService.getActivityList(page);
+        IPage<ActivityInfo> activityList = scienceActivityService.getActivityList(page,activityInfo);
         result.setData(activityList);
         result.setStatus(ResultStatus.SUCCESS.value());
         result.setMsg(ResultMsg.MSG_SUCCESS);
